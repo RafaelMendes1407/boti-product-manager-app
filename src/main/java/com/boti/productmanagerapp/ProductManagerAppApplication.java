f7a1a4fdb5d3b04c819b7350add62f82e5e71c23
Rafael Mendes <rafael_mendes@outlook.com>
2025-07-29 18:43:58 -0300

    first commit

---
+++ src/main/java/com/boti/productmanagerapp/ProductManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.boti.productmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boti/product_manager_app/ProductManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boti.product_manager_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
