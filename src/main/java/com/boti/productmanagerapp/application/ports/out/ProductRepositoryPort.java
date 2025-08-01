946b031e124770f94ada6e13149589add183347c
Rafael Mendes <rafael_mendes@outlook.com>
2025-08-01 00:30:05 -0300

    refact: refacts ProductQueryusecase

--- src/main/java/com/boti/productmanagerapp/application/ports/out/ProductRepositoryPort.java
+++ src/main/java/com/boti/productmanagerapp/application/ports/out/ProductRepositoryPort.java
@@ -1,18 +1,15 @@
 package com.boti.productmanagerapp.application.ports.out;
 
-import com.boti.productmanagerapp.adapters.out.entities.ProductEntity;
+import com.boti.productmanagerapp.application.core.domain.PageResult;
 import com.boti.productmanagerapp.application.core.domain.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepositoryPort {
 
     Product save (Product product);
-    Page<ProductEntity> queryByNameOrPriceRange(String name, BigDecimal minPrice, BigDecimal maxPrice, Pageable page);
+    PageResult<Product> queryByNameOrPriceRange(String name, BigDecimal minPrice, BigDecimal maxPrice, int pageNumber, int pageSize);
     Product findByProductId(long id);
     Long count();
     void deleteAll();
